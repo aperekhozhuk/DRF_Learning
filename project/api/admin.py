07f9beedad4ca85653af4feb8cba0e82ca98ed74
Anton Perekhozhuk <anton.perekhozhuk@gmail.com>
2020-12-27 07:08:46 +0200

    Add Post model

--- project/api/admin.py
+++ project/api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    fields = ('title', 'text')
+    list_display = ('title',)
+    search_fields = ('title',)
+
+
+admin.site.register(Post, PostAdmin)
