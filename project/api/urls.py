3e59f35cb361020a36e48f8f5bb588ac4e5892d7
Anton Perekhozhuk <anton.perekhozhuk@gmail.com>
2021-01-18 02:13:37 +0200

    Add route 'users/<user_id>/posts/<post_id>/comments'

--- project/api/urls.py
+++ project/api/urls.py
@@ -13,9 +13,22 @@ users_router.register(r'posts', views.PostViewSet, basename='user-posts')
 posts_router = routers.NestedSimpleRouter(router, r'posts', lookup='post')
 posts_router.register(r'comments', views.CommentViewSet, basename='post-comments')
 
+user_posts_router = routers.NestedSimpleRouter(
+    users_router,
+    r'posts',
+    lookup='post'
+)
+user_posts_router.register(
+    r'comments',
+    views.CommentViewSet,
+    basename='user-post-comments'
+)
+
+
 urlpatterns = [
     path('', include(router.urls)),
     path('', include(users_router.urls)),
     path('', include(posts_router.urls)),
+    path('', include(user_posts_router.urls)),
     path('auth/', include('rest_framework.urls')),
 ]
